64d2d33fe53c7c6fa5119b6be40d01cee4e85f50
dimarsM142 <dimonars11032003@gmail.com>
2022-12-01 20:46:47 +0200

    mailer

--- helpers/mailHelper.js
+++ helpers/mailHelper.js
@@ -124,7 +124,7 @@ function sendRemindByTask(sendEventArray, eventTitle, calendarTitle) {
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Task notification',
+        subject: 'Chronos. Task notification!',
         html:`
         <html>
             <head>
@@ -255,7 +255,7 @@ function sendRemindByArrangement(sendEventArray, eventTitle, calendarTitle) {
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Arrangement notification',
+        subject: 'Chronos. Arrangement notification!',
         html:`
         <html>
             <head>
@@ -392,7 +392,7 @@ function sendRemindByReminder(sendEventArray, eventTitle, calendarTitle) {
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Reminder notification',
+        subject: 'Chronos. Reminder notification!',
         html:`
         <html>
             <head>
@@ -523,11 +523,125 @@ function createEventNtfc(sendEventArray, calendarTitle, eventType) {
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Prompt',
+        subject: 'Chronos. New event created!',
         html:`
-        <h2>Notification</h2>
-        <p>A new ${eventType} has appeared in the "${calendarTitle}" calendar</p>
-        <br><br><p>This letter does not require a response.</p>
+        <html>
+            <head>
+                <style>
+                    body { 
+                        font-family: 'Didact Gothic', sans-serif;
+                        margin: 0px 40px;
+                    }
+                    p {
+                        padding: 0px;
+                        margin: 0px;
+                        
+                    }
+                    h1 {
+                        text-align: center;
+                        font-size: 36px;
+                        font-family: 'Comfortaa', cursive;
+                        color: green;
+                    }
+                    img {
+                        margin:auto;
+                        width: 100px;
+                        height: 100px;
+                    }
+                    .header {
+                        display: grid;
+                        justify-content: center;
+                        row-gap: 20px;
+                    }
+                    h2 {
+                        font-weight: 700;
+                        font-size: 20px;
+                        opacity: 0.6;
+                        margin-top: 30px;
+                        margin-bottom: 30px;   
+                    }
+                    .text-content {
+                        opacity: 0.6;
+                        padding: 10px 20px;
+                        font-size: 18px;
+                        margin-bottom: 20px;
+                    }
+                    .link {
+                        margin: auto;
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .link a {
+                        text-transform: none;
+                        text-decoration: none;
+                        color: white;
+                        background-color: green;
+                        font-family: 'Comfortaa', cursive;
+                        text-transform: uppercase;
+                        font-size: 30px;
+                        padding: 5px 30px;
+                        transition: 0.4s;
+                        
+                    }
+                    .link a:hover{
+                        background-color: rgb(23, 88, 3);
+                    }
+                    .link a:active{
+                        background-color: rgb(0, 150, 12);
+                        color: #ffffff;
+                        box-shadow: 10px 15px 15px rgb(112, 112, 112);
+                    }
+                    .last-part {
+                        text-align: right;
+                        opacity: 0.7;
+                        font-size: 24px;
+                        margin-top: 40px;
+                        color: green;
+                    }
+                    .text-calendarTitle {
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .text-calendarTitle .title{
+                        margin-right: 10px;
+                        opacity: 0.3;
+                        font-size: 20px;
+                    }
+                    .text-calendarTitle .content{
+                        text-transform: uppercase;
+                        color: green;
+                        font-size: 30px;
+                        font-weight: 700;
+                        font-family: 'Comfortaa', cursive;
+                    }
+                    .text-eventTitle{
+                        opacity: 1;
+                        color: black;
+                        font-size: 24px;
+                        color: green;
+                        font-style: italic;
+                    
+                    }
+                    
+                </style>
+            </head>
+            <body>
+                <div class="header">
+                    <h1>Chronos</h1>
+                    <img className="logo" src='https://www.pngkey.com/png/full/18-180664_calendar-clock-comments-time-and-date-icon-png.png' alt='logo'/>
+                </div>
+                <div>
+                    <h2>Event notification</h2>
+                    <p class="text-calendarTitle"><span class="title">calendar</span><span class="content">${calendarTitle}</span></p>
+                    <p class="text-content">A new event - <span class="text-eventTitle">${eventType}</span> has appeared in this calendar. For more details, see this calendar on the website.</p>
+                        <div class="link">
+                            <a href="https://chron0s.herokuapp.com">Go into website!</a>
+                        </div>
+                
+                    <p class="last-part">Enjoy, the rest of your day!</p>
+                </div>
+            </body>
+        </html>
         `
     }
     mailer(message);
@@ -537,14 +651,179 @@ function changeEventNtfc(sendEventArray, calendarTitle, oldEvent, newEvent) { //
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Prompt',
+        subject: 'Chronos. One event changed!',
         html:`
-        <h2>Notification</h2>
-        <p>${oldEvent.type[0].toUpperCase() + oldEvent.type.slice(1)} "${oldEvent.title}" in calendar "${calendarTitle}" was changed</p>
-        <p>Current information about this:<br></p>
-        <p>Title: ${newEvent.title}, description: ${newEvent.description}, execution date: ${newEvent.execution_date.toISOString().replace('T', ' ').replace('Z', '').slice(0, 16)},
-        ${(newEvent.type !== 'arrangement')?(''):(`duration: ${newEvent.duration},`)} type: ${newEvent.type}</p>
-        <br><br><p>This letter does not require a response.</p>
+        <html>
+        <head>
+            <style>
+                body { 
+                    font-family: 'Didact Gothic', sans-serif;
+                    margin: 0px 40px;
+                }
+                p {
+                    padding: 0px;
+                    margin: 0px;
+                    
+                }
+                h1 {
+                    text-align: center;
+                    font-size: 36px;
+                    font-family: 'Comfortaa', cursive;
+                    color: green;
+                }
+                img {
+                    margin:auto;
+                    width: 100px;
+                    height: 100px;
+                }
+                .header {
+                    display: grid;
+                    justify-content: center;
+                    row-gap: 20px;
+                }
+                h2 {
+                    font-weight: 700;
+                    font-size: 20px;
+                    color: rgba(0, 0, 0, 0.6);
+                    margin-top: 30px;
+                    margin-bottom: 30px;   
+                }
+                h2 .oldEvent-type{
+                    font-weight: 700;
+                    color: black;
+                    text-transform: uppercase;
+                    font-size: 22px;
+                    margin-right: 5px;
+                }
+                h2 .oldEvent-content{
+                    font-size: 24px;
+                    color: green;
+                    font-style: italic;
+                    font-weight: 700;
+                }
+                .text-content {
+                    opacity: 0.6;
+                    padding: 10px 20px;
+                    font-size: 18px;
+                    margin-bottom: 20px;
+                }
+                .link {
+                    margin: auto;
+                    text-align: center;
+                    margin-bottom: 20px;
+                }
+                .link a {
+                    text-transform: none;
+                    text-decoration: none;
+                    color: white;
+                    background-color: green;
+                    font-family: 'Comfortaa', cursive;
+                    text-transform: uppercase;
+                    font-size: 30px;
+                    padding: 5px 30px;
+                    transition: 0.4s;
+                    
+                }
+                .link a:hover{
+                    background-color: rgb(23, 88, 3);
+                }
+                .link a:active{
+                    background-color: rgb(0, 150, 12);
+                    color: #ffffff;
+                    box-shadow: 10px 15px 15px rgb(112, 112, 112);
+                }
+                .last-part {
+                    text-align: right;
+                    opacity: 0.7;
+                    font-size: 24px;
+                    margin-top: 40px;
+                    color: green;
+                }
+                .text-calendarTitle {
+                    text-align: center;
+                    margin-bottom: 20px;
+                }
+                .text-calendarTitle .title{
+                    margin-right: 10px;
+                    opacity: 0.3;
+                    font-size: 20px;
+                }
+                .text-calendarTitle .content{
+                    text-transform: uppercase;
+                    color: green;
+                    font-size: 30px;
+                    font-weight: 700;
+                    font-family: 'Comfortaa', cursive;
+                }
+                .text-eventTitle{
+                    opacity: 1;
+                    color: black;
+                    font-size: 24px;
+                    color: green;
+                    font-style: italic;
+                
+                }
+                .one-event{
+                    border: 3px solid green;
+                    padding: 20px;
+                }
+                .one-event .date{
+                    text-align: right;
+                    opacity: 0.5;
+                    margin-bottom: 10px;
+                }
+                .one-event .type{
+                    text-transform: uppercase;
+                    font-weight: 700;
+                    font-size: 24px;
+                    margin-bottom: 10px;
+                }
+                .one-event .title {
+                    color: green;
+                    font-family: 'Comfortaa', cursive;
+                    text-align: center;
+                    font-size: 24px;
+                    margin-top: 5px;
+                    margin-bottom: 20px;
+                }
+                .one-event .desc {
+                    opacity: 0.7;
+                    font-size: 18px;
+                    margin-bottom: 20px;
+    
+                }
+                .one-event .duration {
+                    font-size: 20px;
+                    text-align: center;
+                }
+                
+            </style>
+        </head>
+        <body>
+            <div class="header">
+                <h1>Chronos</h1>
+                <img className="logo" src='https://www.pngkey.com/png/full/18-180664_calendar-clock-comments-time-and-date-icon-png.png' alt='logo'/>
+            </div>
+            <div>
+                <h2><span class="oldEvent-type">${oldEvent.type}</span> <span class="oldEvent-content">${oldEvent.title}</span> was chaged!</h2>
+                <p class="text-calendarTitle"><span class="title">calendar</span><span class="content">${calendarTitle}</span></p>
+                <p class="text-content">The new look of this event:</p>
+                <div class="one-event">
+                    <p class="date">${newEvent.execution_date.toISOString().replace('T', ' ').replace('Z', '').slice(0, 16)}</p>
+                    <p class="type">${newEvent.type}</p>
+                    <p class="title">${newEvent.title}</p> 
+                    <p class="desc">${newEvent.description}</p>
+                   
+                    <p class="duration">Lasts ${(newEvent.type !== 'arrangement')?(''):(newEvent.duration / 3600)}} hours</p>
+                    
+                </div>
+                <div class="link">
+                    <a href="https://chron0s.herokuapp.com">Go into website!</a>
+                </div>
+                <p class="last-part">Enjoy, the rest of your day!</p>
+            </div>
+        </body>
+    </html>
         `
     }
     mailer(message);
@@ -554,12 +833,131 @@ function deleteEventNtfc(sendEventArray, calendarTitle, eventTitle, eventType) {
     const message = {
         from: 'mostlycloudy220@gmail.com',
         to: `${sendEventArray}`,
-        subject: 'Prompt',
+        subject: 'Chronos. Canceled event!',
         html:`
-        <h2>Notification</h2>
-        <p>${eventType[0].toUpperCase() + eventType.slice(1)} "${eventTitle}" in calendar "${calendarTitle}" was canceled</p>
-        <br><br><p>This letter does not require a response.</p>
-        `
+        <html>
+            <head>
+                <style>
+                    body { 
+                        font-family: 'Didact Gothic', sans-serif;
+                        margin: 0px 40px;
+                    }
+                    p {
+                        padding: 0px;
+                        margin: 0px;
+                        
+                    }
+                    h1 {
+                        text-align: center;
+                        font-size: 36px;
+                        font-family: 'Comfortaa', cursive;
+                        color: green;
+                    }
+                    img {
+                        margin:auto;
+                        width: 100px;
+                        height: 100px;
+                    }
+                    .header {
+                        display: grid;
+                        justify-content: center;
+                        row-gap: 20px;
+                    }
+                    h2 {
+                        font-weight: 700;
+                        font-size: 20px;
+                        opacity: 0.6;
+                        margin-top: 30px;
+                        margin-bottom: 30px;   
+                    }
+                    .text-content {
+                        color: rgba(0, 0, 0, 0.7);
+                        padding: 10px 20px;
+                        font-size: 18px;
+                        margin-bottom: 20px;
+                    }
+                    .link {
+                        margin: auto;
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .link a {
+                        text-transform: none;
+                        text-decoration: none;
+                        color: white;
+                        background-color: green;
+                        font-family: 'Comfortaa', cursive;
+                        text-transform: uppercase;
+                        font-size: 30px;
+                        padding: 5px 30px;
+                        transition: 0.4s;
+                        
+                    }
+                    .link a:hover{
+                        background-color: rgb(23, 88, 3);
+                    }
+                    .link a:active{
+                        background-color: rgb(0, 150, 12);
+                        color: #ffffff;
+                        box-shadow: 10px 15px 15px rgb(112, 112, 112);
+                    }
+                    .last-part {
+                        text-align: right;
+                        opacity: 0.7;
+                        font-size: 24px;
+                        margin-top: 40px;
+                        color: green;
+                    }
+                    .text-calendarTitle {
+                        text-align: center;
+                        margin-bottom: 20px;
+                    }
+                    .text-calendarTitle .title{
+                        margin-right: 10px;
+                        opacity: 0.3;
+                        font-size: 20px;
+                    }
+                    .text-calendarTitle .content{
+                        text-transform: uppercase;
+                        color: green;
+                        font-size: 30px;
+                        font-weight: 700;
+                        font-family: 'Comfortaa', cursive;
+                    }
+                    .text-content .text-eventType{
+                        font-weight: 700;
+                        color: black;
+                        text-transform: uppercase;
+                        font-size: 24px;
+                    }
+                    .text-eventTitle{
+                        opacity: 1;
+                        font-size: 24px;
+                        color: green;
+                        font-style: italic;
+                        font-weight: 700;
+                    }
+                    
+                </style>
+            </head>
+            <body>
+                <div class="header">
+                    <h1>Chronos</h1>
+                    <img className="logo" src='https://www.pngkey.com/png/full/18-180664_calendar-clock-comments-time-and-date-icon-png.png' alt='logo'/>
+                </div>
+                <div>
+                    <h2>Event notification</h2>
+                    <p class="text-calendarTitle"><span class="title">calendar</span><span class="content">${calendarTitle}</span></p>
+                    <p class="text-content"><span class="text-eventType">${eventType}</span> <span class="text-eventTitle">${eventTitle}</span> in this calendar was canceled :( For more details, see this calendar on the website.</p>
+                        <div class="link">
+                            <a href="https://chron0s.herokuapp.com">Go into website!</a>
+                        </div>
+                
+                    <p class="last-part">Enjoy, the rest of your day!</p>
+                </div>
+            </body>
+        </html>
+        `,
     }
     mailer(message);
 }
